e91a33c8794aa8b138c31e344711087c1ac087a2
松井永 <eiciemm@gmail.com>
2025-01-29 21:36:12 +0900

    掛け算を追加

--- src/hooks/useCalculate.test.ts
+++ src/hooks/useCalculate.test.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect } from '@jest/globals'
 import { useCalculate } from './useCalculate'
 
-const { add, subtract } = useCalculate()
+const { add, subtract, multiple } = useCalculate()
 
 describe('useCalculate', () => {
   test('add 1 + 2 to equal 3', () => {
@@ -10,5 +10,9 @@ describe('useCalculate', () => {
 
   test('subtract 10 - 2 to equal 8', () => {
     expect(subtract(10, 2)).toBe(8)
+  })
+
+  test('subtract 15 * 2 to equal 30', () => {
+    expect(multiple(15, 2)).toBe(30)
   })
 })
